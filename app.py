ed916f5dafb7fa48662b73509652208e1d35efd0
takaiakio <akio.takai@alpsalpine.com>
2023-01-17 09:04:17 +0900

    インストールしてないけど大丈夫？

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ x
 import pandas as pd
 df = pd.DataFrame({'col1': [1,2,3]})
 df
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+fig
