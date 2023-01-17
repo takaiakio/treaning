32c7718665c21ddd222919ec49520abaa03eb5e3
takaiakio <akio.takai@alpsalpine.com>
2023-01-17 15:37:09 +0900

    また忘れた

--- Home.py
+++ Home.py
@@ -1,35 +1,12 @@
 import streamlit as st
+genre = st.radio(
+     "What's your favorite movie genre",
+     ('Comedy', 'Drama', 'Documentary'))
 
-st.title('もう何度目かのStreamlit!')
-st.markdown(
-    '''
-    私はアプリ作成を練習しています!
-    '''
-)
-with st.sidebar:
-    st.subheader('News!🆕')
-    st.text('画像認識AIが追加されました！')
-
-
-import plotly.graph_objs as go
-
-animals = ['giraffes', 'orangutans', 'monkeys']
-populations = [20, 14, 23]
-
-fig = go.Figure(data=[go.Bar(x=animals, y=populations)])
-
-fig.update_layout(
-    xaxis = dict(
-        tickangle = 0,
-        title_text = "Animal",
-        title_font = {"size": 20},
-        title_standoff = 25),
-    yaxis = dict(
-        title_text = "Populations",
-        title_standoff = 25),
-    title ='Title')
-
-st.plotly_chart(fig, use_container_width=True)
+if genre == 'Comedy':
+     st.write('You selected comedy.')
+else:
+     st.write("You didn't select comedy.")
 
 import altair as alt
 from vega_datasets import data
