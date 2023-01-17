fb73e24772cd798f1e7bd73ce57bbb8b57d37cc2
takaiakio <akio.takai@alpsalpine.com>
2023-01-17 15:23:58 +0900

    まるで別もの

--- Home.py
+++ Home.py
@@ -30,3 +30,20 @@ fig.update_layout(
     title ='Title')
 
 st.plotly_chart(fig, use_container_width=True)
+
+import altair as alt
+from vega_datasets import data
+
+source = data.cars()
+
+fig = alt.Chart(source).mark_circle(size=60).encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin',
+    tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
+).properties(
+    width=500,
+    height=500
+).interactive()
+
+st.write(fig)
