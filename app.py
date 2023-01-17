521b87ba30ca1de689f179377d083388d1568753
takaiakio <akio.takai@alpsalpine.com>
2023-01-17 10:28:21 +0900

    Checkboxで表示

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ import numpy as np
 import pandas as pd
 
 st.title('My first app')
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c']
+    )
+    st.line_chart(chart_data)
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
