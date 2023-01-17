2ef9930463a56e59bbffd477dc91994c9ffb09f9
takaiakio <akio.takai@alpsalpine.com>
2023-01-17 09:20:30 +0900

    istallは関係ないな

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-x = 10
-x
-
-import pandas as pd
-df = pd.DataFrame({'col1': [1,2,3]})
-df
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-fig
-
 import streamlit as st
 
 st.radio("好きなマイケルは？", ('ジャクソン', 'ジョーダン', 'ホフマン'))
