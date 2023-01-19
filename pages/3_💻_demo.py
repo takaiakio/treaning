778dc96ce8278c50afbd1f4cefee94db4914b091
takaiakio <akio.takai@alpsalpine.com>
2023-01-19 14:27:39 +0900

    また別なやつ

--- pages/3_💻_demo.py
+++ pages/3_💻_demo.py
@@ -1,63 +1,14 @@
-#import streamlit as st
-#import pandas as pd
-
-#For Excel File
-#df = pd.read_excel('world-happiness-report-2021.xlxs')
-
-#For CSV File
-#df = pd.read_csv("world-happiness-report-2021.csv")
-
-#uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
-
-#if uploaded_file is not None:
-#    df = pd.read_csv(
-#        uploaded_file,
-#        engine='python',
-#        na_values='-',
-#        skipinitialspace=True,
-#        encoding="shift-jis"
-#        )
-
-#st.markdown('### アクセスログ（先頭5件）')
-#st.write(df.head(5))
-
-#st.dataframe(df)
-
-
-
 import streamlit as st
-import altair as alt
-from vega_datasets import data
-
-st.markdown(
-    '''
-    StreamlitでAltairのインタラクティブなグラフを表示する方法
-    '''
-)
-
-# データサンプルを取得
-def get_data():
-    source = data.stocks()
-    source = source[source.date.gt("2004-01-01")]
-    return source
-
-source = get_data()
-
-chart = alt.Chart(source).mark_line().encode(
-    x="date:T",
-    y="price",
-    color="symbol"
-)
-
-st.altair_chart(chart, use_container_width=True)
-
-chart = alt.Chart(source).mark_line().encode(
-    x="date:T",
-    y="price",
-    color=alt.Color("symbol", sort=None)
-)
-
-st.altair_chart(chart, use_container_width=True)
+uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
 
 
+import pandas as pd
+if uploaded_file is not None:
+    df = pd.read_csv(
+        uploaded_file,
+        engine='python',
+        na_values='-',
+        )
 
+st.markdown('### アクセスログ（先頭5件）')
+st.write(df.head(5))
