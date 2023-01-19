ce4d0fa98eb39693e54163eaa26596d99ce6fc48
takaiakio <akio.takai@alpsalpine.com>
2023-01-19 15:47:53 +0900

    本日最後

--- pages/3_💻_demo.py
+++ pages/3_💻_demo.py
@@ -14,6 +14,42 @@ if uploaded_file is not None:
 st.dataframe(df)
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# テキストの描画
+st.title('Sample page')
+st.header('Header')
+st.markdown('''
+チョコボールの内容量
+
+- ピーナッツ
+  - 28g
+- いちご
+  - 25g
+''')
+
+@st.cache
+def rand_df(r=10, c=5):
+    df = pd.DataFrame(
+        np.random.randn(r, c),
+        columns=('col %d' % i for i in range(c)))
+    return df
+dataframe = rand_df(c=3, r=10)
+
+# 表の描画
+st.dataframe(dataframe.head(n=3))
+# チャートの描画
+st.line_chart(dataframe)
+# widget
+lst_flavors = ['ピーナッツ', 'いちご']
+select_taste = st.selectbox('Flavor', lst_flavors)
+# Sidebar
+st.sidebar.header('サイドバー')
+p = st.sidebar.slider('確率の設定', min_value=0.0, max_value=1.0, value=0.8)
+st.sidebar.write(f'設定値は {p} です')
+
+
 
 st.line_chart(data = df,width = 0,height = 0,use_container_width = True)
 
