87141da896d5af64012bd1f2e6df3861f6d02eb6
takaiakio <akio.takai@alpsalpine.com>
2023-01-23 16:10:31 +0900

    bom

--- pages/Correspondence.py
+++ pages/Correspondence.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import japanize_matplotlib
+import base64
 
 st.title('コレスポンデンス分析')
 
@@ -24,18 +25,32 @@ if uploaded_file is not None:
 st.markdown('### ↓アクセスログ（先頭5件）')
 st.dataframe(df)
 
+st.table(df)
+csv = df.to_csv(index=False)  
+
+# utf-8
+'''
+b64 = base64.b64encode(csv.encode()).decode()
+href = f'<a href="data:application/octet-stream;base64,{b64}" download="result_utf-8.csv">Download Link</a>'
+st.markdown(f"CSVファイルのダウンロード(utf-8):  {href}", unsafe_allow_html=True)
+'''
+# utf-8(BOM)
+b64 = base64.b64encode(csv.encode('utf-8-sig')).decode()
+href = f'<a href="data:application/octet-stream;base64,{b64}" download="result_utf-8-sig.csv">Download Link</a>'
+st.markdown(f"CSVファイルのダウンロード(utf-8 BOM):  {href}", unsafe_allow_html=True)
 
 mca_counts = mca.MCA(df, benzecri=False)#benzecri=Falseにしないと固有値が0になってしまう
 rws = mca_counts.fs_r(N=2) #商品（行）の成分スコア　Nは保持する成分の数
 cols = mca_counts.fs_c(N=2) #イメージ（列）の成分スコア　Nは保持する成分の数
+
 
 fig = plt.figure() #わからない2
 ax = plt.axes() #わからない3
 #グラフのレイアウトを設定
 plt.axhline(0, color='gray') #横線
 plt.axvline(0, color='gray') #縦線
-japanize_matplotlib.xlabel('成分1', fontname='MS Gothic') #X軸のラベル fontname：日本語の文字化け防止にフォントを指定
-japanize_matplotlib.ylabel('成分2', fontname='MS Gothic') #Y軸のラベル
+plt.xlabel('成分1', fontname='MS Gothic') #X軸のラベル fontname：日本語の文字化け防止にフォントを指定
+plt.ylabel('成分2', fontname='MS Gothic') #Y軸のラベル
 
 
 #商品（行）のプロット
