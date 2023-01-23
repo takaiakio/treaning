dfbd3bae4e3bf7bbc30abfc0fa6401e08a3c834c
takaiakio <akio.takai@alpsalpine.com>
2023-01-23 16:17:52 +0900

    え～

--- pages/Correspondence.py
+++ pages/Correspondence.py
@@ -30,7 +30,6 @@ st.table(df)
 csv = df.to_csv(index=False)  
 
 # utf-8
-
 b64 = base64.b64encode(csv.encode()).decode()
 href = f'<a href="data:application/octet-stream;base64,{b64}" download="result_utf-8.csv">Download Link</a>'
 st.markdown(f"CSVファイルのダウンロード(utf-8):  {href}", unsafe_allow_html=True)
