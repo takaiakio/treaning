05d055f6314540a9b87d3dc3a78a8a0f1e71518a
takaiakio <akio.takai@alpsalpine.com>
2023-01-24 09:14:32 +0900

    font

--- pages/Correspondence.py
+++ pages/Correspondence.py
@@ -49,21 +49,21 @@ ax = plt.axes() #わからない3
 #グラフのレイアウトを設定
 plt.axhline(0, color='gray') #横線
 plt.axvline(0, color='gray') #縦線
-plt.xlabel('成分1', prop={"family":"MS Gothic"}) #X軸のラベル fontname：日本語の文字化け防止にフォントを指定
-plt.ylabel('成分2', prop={"family":"MS Gothic"}) #Y軸のラベル
+plt.xlabel('成分1', fontname={"family":"MS Gothic"}) #X軸のラベル fontname：日本語の文字化け防止にフォントを指定
+plt.ylabel('成分2', fontname={"family":"MS Gothic"}) #Y軸のラベル
 
 
 #商品（行）のプロット
 plt.scatter(rws[:,0], rws[:,1], c='r',marker='s') #c:色、marker:マーク
 labels = df.index
 for label,x,y in zip(labels,rws[:,0],rws[:,1]):
-    plt.annotate(label, xy=(x, y), prop={"family":"MS Gothic"},color = 'b')
+    plt.annotate(label, xy=(x, y), fontname={"family":"MS Gothic"},color = 'b')
 
 
 #イメージ（列）のプロット
 plt.scatter(cols[:,0], cols[:,1], c='c',marker='o',s=5)
 labels = df.columns
 for label,x,y in zip(labels,cols[:,0],cols[:,1]):
-    plt.annotate(label, xy=(x, y), prop={"family":"MS Gothic"},fontsize=10,color = 'c')
+    plt.annotate(label, xy=(x, y), fontname={"family":"MS Gothic"},fontsize=10,color = 'c')
 
 st.pyplot(fig)
