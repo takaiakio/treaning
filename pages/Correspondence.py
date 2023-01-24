6b00fe9fefe1e5ec315e9e256b0b55353fad50a3
takaiakio <akio.takai@alpsalpine.com>
2023-01-24 09:47:12 +0900

    MS Goshic

--- pages/Correspondence.py
+++ pages/Correspondence.py
@@ -3,8 +3,8 @@ import mca
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
-import japanize_matplotlib
-import base64
+#import japanize_matplotlib
+#import base64
 plt.rcParams['font.family'] = 'MS Gothic'
 
 st.title('コレスポンデンス分析')
@@ -26,20 +26,7 @@ if uploaded_file is not None:
 st.markdown('### ↓アクセスログ')
 st.dataframe(df)
 
-'''
-st.table(df)
-csv = df.to_csv(index=False)  
 
-# utf-8
-b64 = base64.b64encode(csv.encode()).decode()
-href = f'<a href="data:application/octet-stream;base64,{b64}" download="result_utf-8.csv">Download Link</a>'
-st.markdown(f"CSVファイルのダウンロード(utf-8):  {href}", unsafe_allow_html=True)
-
-# utf-8(BOM)
-b64 = base64.b64encode(csv.encode('utf-8-sig')).decode()
-href = f'<a href="data:application/octet-stream;base64,{b64}" download="result_utf-8-sig.csv">Download Link</a>'
-st.markdown(f"CSVファイルのダウンロード(utf-8 BOM):  {href}", unsafe_allow_html=True)
-'''
 mca_counts = mca.MCA(df, benzecri=False)#benzecri=Falseにしないと固有値が0になってしまう
 rws = mca_counts.fs_r(N=2) #商品（行）の成分スコア　Nは保持する成分の数
 cols = mca_counts.fs_c(N=2) #イメージ（列）の成分スコア　Nは保持する成分の数
@@ -50,21 +37,21 @@ ax = plt.axes() #わからない3
 #グラフのレイアウトを設定
 plt.axhline(0, color='gray') #横線
 plt.axvline(0, color='gray') #縦線
-plt.xlabel('成分1', fontname="IPAexGothic",fontsize=15) #X軸のラベル fontname：日本語の文字化け防止にフォントを指定
-plt.ylabel('成分2', fontname="IPAexGothic",fontsize=15) #Y軸のラベル
+plt.xlabel('成分1', fontname="MS Gothic",fontsize=15) #X軸のラベル fontname：日本語の文字化け防止にフォントを指定
+plt.ylabel('成分2', fontname="MS Gothic",fontsize=15) #Y軸のラベル
 
 
 #商品（行）のプロット
 plt.scatter(rws[:,0], rws[:,1], c='r',marker='s') #c:色、marker:マーク
 labels = df.index
 for label,x,y in zip(labels,rws[:,0],rws[:,1]):
-    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=10,color = 'b')
+    plt.annotate(label, xy=(x, y), fontname="MS Gothic",fontsize=10,color = 'b')
 
 
 #イメージ（列）のプロット
 plt.scatter(cols[:,0], cols[:,1], c='c',marker='o',s=5)
 labels = df.columns
 for label,x,y in zip(labels,cols[:,0],cols[:,1]):
-    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=10,color = 'c')
+    plt.annotate(label, xy=(x, y), fontname="MS Gothic",fontsize=10,color = 'c')
 
 st.pyplot(fig)
