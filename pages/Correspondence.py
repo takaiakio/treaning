d334fa5e3ae87eb230ffee37a8d887ff56934f0f
takaiakio <akio.takai@alpsalpine.com>
2023-02-07 12:10:27 +0900

    適正評価

--- pages/Correspondence.py
+++ pages/Correspondence.py
@@ -51,13 +51,13 @@ plt.ylim(bottom=-1, top=1)
 # グラフタイトル
 plt.title('コレポン出力', fontname="IPAexGothic",fontsize=15)
 
-'''
+
 #商品（行）のプロット
 plt.scatter(rws[:,0], rws[:,1], c='r',marker='s',s=10) #c:色、marker:マーク
 labels = df.index
 for label,x,y in zip(labels,rws[:,0],rws[:,1]):
     plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=15,color = 'b')
-'''
+
 
 #イメージ（列）のプロット
 plt.scatter(cols[:,0], cols[:,1], c='c',marker='o',s=20)
