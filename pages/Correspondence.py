76fbaf895cc503cbf1ac88785da21ed1111d8e06
takaiakio <akio.takai@alpsalpine.com>
2023-02-07 14:07:36 +0900

    範囲変更

--- pages/Correspondence.py
+++ pages/Correspondence.py
@@ -45,8 +45,8 @@ plt.ylabel('成分2', fontname="IPAexGothic",fontsize=15) #Y軸のラベル
 plt.grid(True)
 
 # 軸範囲
-plt.xlim(left=-2, right=2)
-plt.ylim(bottom=-2, top=2)
+plt.xlim(left=-1.5, right=1.5)
+plt.ylim(bottom=-1.5, top=1.5)
 
 # グラフタイトル
 plt.title('コレポン出力', fontname="IPAexGothic",fontsize=15)
