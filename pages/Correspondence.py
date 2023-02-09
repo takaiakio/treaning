5fea36370629b77ee778184341adb8a6809a2d81
takaiakio <akio.takai@alpsalpine.com>
2023-02-09 14:39:19 +0900

    入れ替え

--- pages/Correspondence.py
+++ pages/Correspondence.py
@@ -56,14 +56,14 @@ plt.title('コレポン出力', fontname="IPAexGothic",fontsize=15)
 plt.scatter(rws[:,0], rws[:,1], c='r',marker='s',s=100) #c:色、marker:マーク
 labels = df.index
 for label,x,y in zip(labels,rws[:,0],rws[:,1]):
-    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=30,color = 'b')
+    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=15,color = 'b')
 
 
 #イメージ（列）のプロット
 plt.scatter(cols[:,0], cols[:,1], c='c',marker='o',s=100)
 labels = df.columns
 for label,x,y in zip(labels,cols[:,0],cols[:,1]):
-    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=15,color = 'c')
+    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=30,color = 'c')
 
 
 
