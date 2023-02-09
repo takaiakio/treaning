f0dd3654f6bc5b0a28c57f04e46f6d651a20c130
takaiakio <akio.takai@alpsalpine.com>
2023-02-09 14:35:57 +0900

    文変更

--- pages/Correspondence.py
+++ pages/Correspondence.py
@@ -8,9 +8,9 @@ import japanize_matplotlib
 #import base64
 plt.rcParams['font.family'] = 'IPAexGothic'
 
-st.title('コレスポンデンス分析')
+st.title('適性分析')
 
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+uploaded_file = st.file_uploader("クロス表.csvをアップロードしてください。")
 
 if uploaded_file is not None:
     df = pd.read_csv(
@@ -38,8 +38,8 @@ ax = plt.axes() #わからない3
 #グラフのレイアウトを設定
 plt.axhline(0, color='gray') #横線
 plt.axvline(0, color='gray') #縦線
-plt.xlabel('成分1', fontname="IPAexGothic",fontsize=15) #X軸のラベル fontname：日本語の文字化け防止にフォントを指定
-plt.ylabel('成分2', fontname="IPAexGothic",fontsize=15) #Y軸のラベル
+plt.xlabel('第1成分', fontname="IPAexGothic",fontsize=20) #X軸のラベル fontname：日本語の文字化け防止にフォントを指定
+plt.ylabel('第2成分', fontname="IPAexGothic",fontsize=20) #Y軸のラベル
 
 # grid表示
 plt.grid(True)
@@ -56,14 +56,14 @@ plt.title('コレポン出力', fontname="IPAexGothic",fontsize=15)
 plt.scatter(rws[:,0], rws[:,1], c='r',marker='s',s=100) #c:色、marker:マーク
 labels = df.index
 for label,x,y in zip(labels,rws[:,0],rws[:,1]):
-    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=10,color = 'b')
+    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=15,color = 'b')
 
 
 #イメージ（列）のプロット
 plt.scatter(cols[:,0], cols[:,1], c='c',marker='o',s=100)
 labels = df.columns
 for label,x,y in zip(labels,cols[:,0],cols[:,1]):
-    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=10,color = 'c')
+    plt.annotate(label, xy=(x, y), fontname="IPAexGothic",fontsize=15,color = 'c')
 
 
 
